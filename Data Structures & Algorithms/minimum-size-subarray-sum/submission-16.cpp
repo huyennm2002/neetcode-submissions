b7855100626cba0d10d80b6258d5eea262f04a3e
Minh Huyen T Nguyen <52208231+huyennm2002@users.noreply.github.com>
2026-08-23 22:46:18 -0400

    Add: minimum-size-subarray-sum - submission-16

---
+++ Data Structures & Algorithms/minimum-size-subarray-sum/submission-16.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int minSubArrayLen(int target, vector<int>& nums) {
+        int sum = 0;
+        int begin = 0;
+        int end = 0;
+        int result = nums.size() + 1;
+        while (end <= nums.size()) {
+            while (sum >= target) {
+                if (end - begin + 1 < result) {
+                    result = end - begin + 1;
+                }
+                sum -= nums[begin];
+                begin++;
+            } elseif (end < nums.size()) {
+                sum += nums[end];
+                end++;
+            }
+        }
+        if (result < nums.size() + 1) {
+            return result;
+        } else {
+            return 0;
+        }
+    }
+};
